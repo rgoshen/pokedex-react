98840d3522e7cc95154b676aa6c8ab964eabfa43
Richard Goshen <rick.goshen@gmail.com>
2022-11-14 21:51:15 -0700

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card({ data }) {
+  return (
+    <div className='card'>
+      <div className='img-container'>
+        <div className='img-inner'>
+          <div className='inner-skew'>
+            <img
+              src='https://picsum.photos/400/300'
+              alt='test'
+              className='header-img'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='card-body'>
+        <h2>{data.name.english}</h2>
+        <ul>
+          <li>HP: {data.base.HP}</li>
+          <li>Attack: {data.base.Attack}</li>
+          <li>Defense: {data.base.Defense}</li>
+          <li>Sp. Attack: {data.base['Sp. Attack']}</li>
+          <li>Sp. Defense: {data.base['Sp. Defense']}</li>
+          <li>Speed: {data.base.Speed}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/PokemonBoard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PokeBoard() {
-  return (
-    <div className='poke-board'>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
