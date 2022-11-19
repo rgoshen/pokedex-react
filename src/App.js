5311b6dabb8c8505307df35076e82fa944bedb21
Richard Goshen <rick.goshen@gmail.com>
2022-11-19 14:32:07 -0700

    cleanup initial styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Board from './components/Board';
-import './assets/css/styles.css';
 import './App.css';
-import bgImage from './assets/images/pokemon-bg.jpg';
 import logo from './assets/images/pokeball.png';
 
 function App() {
   return (
-    <div
-      className='App bg-wrapper'
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
+    <div className='App bg-wrapper'>
       <Navbar
         logo={logo}
         alt='pokeball'
