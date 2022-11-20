78725761b2f34e2dc602a28de8667996317f5d4a
Richard Goshen <rick.goshen@gmail.com>
2022-11-20 10:13:54 -0700

    remove bad attributes row component

--- src/components/AttributeRow.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './AttributeRow.css';
-
-export default function AttributeRow({ attrName, attrValue }) {
-  return <li className='skills-row'>{`${attrName}: ${attrValue}`}</li>;
-}
